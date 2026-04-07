a6728ed6f3270e5fe8e18eadce4249d2721a4b3b
Chris LaPointe <chrislapointe@me.com>
2026-04-07 13:18:29 -0400

    Update NotifiCLI to v1.4.0

--- Casks/notificli.rb
+++ Casks/notificli.rb
@@ -1,6 +1,6 @@
 cask "notificli" do
-  version "1.1.0"
-  sha256 "c0c0169aa42946ff2d8c52f73c6ef70a48bf160e2fe96a8af49f107a5efa541b"
+  version "1.4.0"
+  sha256 "d3d8b53ba409d82612bf391a8cd957f7e560c69012cf001d5ce22b6e284290f1"
 
   url "https://github.com/saihgupr/NotifiCLI/archive/refs/tags/v#{version}.tar.gz"
   name "NotifiCLI"
